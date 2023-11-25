834d40c85195a48f2cf82fade0fcde148b0d94b7
Tarek Daaboul <tarek.daaboul@ingka.com>
2023-11-25 18:29:39 +0200

    all

--- src/controllers/utilController.js
+++ src/controllers/utilController.js
@@ -1,9 +1,9 @@
 async function getDateTime(req, res) {
   try {
     const date = new Date();
-    const datetime = `${date.getHours()}:${date.getMinutes()}:${date
-      .getSeconds()
-      .toFixed(2)}`;
+    const s = date.getSeconds();
+    const sDisplay = s > 0 ? (s < 10 ? "0" + s : s) : "00";
+    const datetime = `${date.getHours()}:${date.getMinutes()}:${sDisplay}`;
     res.status(201).json({ dateTime: datetime });
   } catch (error) {
     res.status(400).json({ error: error.message });
